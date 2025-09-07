8b8086fbba642b093088a220418cc97f4e98e2af
jonathan-yaary <jonathan.yaary@gmail.com>
2025-09-07 17:46:28 +0300

    try to has router

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App";
 import Home from "./components/home/Home";
@@ -8,12 +8,12 @@ import Cocktails from "./components/cocktails/Cocktails";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path={import.meta.env.VITE_BASE_URL} element={<App />} />
         <Route path={import.meta.env.VITE_BASE_URL + "home"} element={<Home />} />
         <Route path={import.meta.env.VITE_BASE_URL + "cocktails"} element={<Cocktails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
