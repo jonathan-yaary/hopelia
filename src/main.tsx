c1d8afbc4dacf89f6bd30a646f5a108300597998
jonathan-yaary <jonathan.yaary@gmail.com>
2025-09-07 13:15:20 +0300

    build info

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
-import App from "./App.tsx";
-import Home from "./components/home/Home.tsx";
+import App from "./App";
+import Home from "./components/home/Home";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
