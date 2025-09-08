efc0547b4fab8bccc551d7b0ec9ea47df43f71f2
jonathan-yaary <jonathan.yaary@gmail.com>
2025-09-08 13:04:30 +0300

    fix ts error

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 import "./index.css";
 import App from "./App";
-import Home from "./components/home/Home";
-import Cocktails from "./components/cocktails/Cocktails";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
